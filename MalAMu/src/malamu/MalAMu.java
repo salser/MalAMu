15cce56359a69a989758dccf7ad99e26eed67486
Henry Steven Salazar <Henry Steven Salazar@DESKTOP-099MEOL>
2017-08-25 19:21:12 -0500

    commit inicial

---
+++ MalAMu/src/malamu/MalAMu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package malamu;
+
+/**
+ *
+ * @author Henry Steven Salazar
+ */
+public class MalAMu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

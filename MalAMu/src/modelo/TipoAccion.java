20412b6498ee54cf1de325d10456a3a16f1ab24d
Henry Steven Salazar <Henry Steven Salazar@DESKTOP-099MEOL>
2017-08-28 18:57:49 -0500

    Clases de lógica del juego finalizadas con sus respectivos comentarios

--- MalAMu/src/modelo/TipoAccion.java
+++ MalAMu/src/modelo/TipoAccion.java
@@ -1,7 +1,7 @@
 package modelo;
 
 /**
- *
+ * Enumeración que indica el tipo de jugada en un turno que un jugador hace: ATAQUE,DEFENSA, RECARGA
  * @author Henry Salazar, David Villamizar, Juan Espinosa
  */
 public enum TipoAccion {

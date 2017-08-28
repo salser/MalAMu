76428373a75664e610cbfa4dc733e28b5158af83
davl3232 <davl3232@gmail.com>
2017-08-28 18:40:21 -0500

    Creación clase servidor

---
+++ MalAMu/src/modelo/Servidor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.net.InetAddress;
+
+/**
+ * Responde peticiones de los clientes.
+ * Cada turno, recibe todas las jugadas de los clientes y envía una respuesta de
+ * vuelta con una nueva lista de jugadores que representa la . 
+ * 
+ * @author davl3232
+ */
+public class Servidor {
+	InetAddress direccion;
+}

3aad8f024d085bca0307412d2f6339e2a2c6d240
davl3232 <davl3232@gmail.com>
2017-09-13 18:27:49 -0500

    Cambios en servidor.

--- MalAMu/src/malamu/Servidor.java
+++ MalAMu/src/malamu/Servidor.java
@@ -6,6 +6,12 @@
 package malamu;
 
 import java.net.InetAddress;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Queue;
+import modelo.Partida;
 
 /**
  * Responde peticiones de los clientes.
@@ -15,5 +21,76 @@ import java.net.InetAddress;
  * @author davl3232
  */
 public class Servidor {
-	InetAddress direccion;
+	/**
+	 * Dirección IP del servidor.
+	 */
+	protected InetAddress direccion;
+	
+	/**
+	 * Tiempo de inactividad tomado desde la recepción del último mensaje desde este servidor.
+	 */
+	protected LocalDateTime tiempoInicioInactividad;
+	
+	/**
+	 * Tiempo de inactividad máximo que se permite antes de terminar la conexión con este servidor.
+	 */
+	protected Duration duracionMaximaInactividad;
+	
+	/**
+	 * Tiempo tomado al iniciar el emparejamiento para una nueva partida.
+	 */
+	protected LocalDateTime tiempoInicioEmparejamiento;
+	
+	/**
+	 * Tiempo de emparejamiento máximo que se permite antes de iniciar una partida con los clientes seleccionados hasta ese momento.
+	 */
+	protected Duration duracionMaximaEmparejamiento;
+	
+	/**
+	 * Tiene la partida actual.
+	 */
+	protected Partida partida;
+	
+	/**
+	 * Tiene la cola con los clientes que desean unirse a la nueva partida.
+	 */
+	protected Queue<Cliente> colaClientes;
+
+	public Servidor(InetAddress direccion, LocalDateTime tiempoInicioInactividad, Duration duracionMaximaInactividad, LocalDateTime tiempoInicioEmparejamiento, Duration duracionMaximaEmparejamiento) {
+		this.direccion = direccion;
+		this.tiempoInicioInactividad = tiempoInicioInactividad;
+		this.duracionMaximaInactividad = duracionMaximaInactividad;
+		this.tiempoInicioEmparejamiento = tiempoInicioEmparejamiento;
+		this.duracionMaximaEmparejamiento = duracionMaximaEmparejamiento;
+	}
+	
+	public void calcularTurno() {
+		
+	}
+	
+	public void iniciarPartida() {
+		while (!colaClientes.isEmpty()) {
+			
+		}
+	}
+	
+	public void recibirJugadas() {
+		
+	}
+	
+	public void enviarResultados() {
+		
+	}
+	
+	public void matarDormidos() {
+		
+	}
+	
+	public void recibirJugadores() {
+		
+	}
+	
+	public boolean pedirConfirmacion(Cliente c) {
+		return true;
+	}
 }
